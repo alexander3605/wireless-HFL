523d7dd713e8b1df497113cb91739deff6830d71
alexander3605 <alessandro.serena3605@gmail.com>
2020-05-13 00:52:58 +0100

    edit neural network architecture for CIFAR

--- src/nn_classes.py
+++ src/nn_classes.py
@@ -99,17 +99,19 @@ class SimpleCifarNetNoBN(nn.Module):
 class MyCifarNet(nn.Module):
     def __init__(self):
         super(MyCifarNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 32, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 64, 3)
+        self.fc1 = nn.Linear(64 * 2 * 2, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 64 * 2 * 2)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
